6de659ec4ef6886489a745a5eddd89bd0fe31748
Jake Wellian <104726391+JakeWellian@users.noreply.github.com>
2026-02-01 12:12:39 +0100

    Add files via upload

--- CameraController.cs
+++ CameraController.cs
@@ -4,15 +4,36 @@ public class CameraController : MonoBehaviour
 {
     private Transform target;
 
+    public Transform clampMin, clampMax;
+
+    public Camera cam;
+    private float halfwidth, halfheight;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = FindAnyObjectByType<PlayerController>().transform;
+
+        clampMin.SetParent(null);
+        clampMax.SetParent(null);
+
+        cam = GetComponent<Camera>();
+        halfheight = cam.orthographicSize;
+        halfwidth = cam.orthographicSize * cam.aspect;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        Vector3 clampedPosition = transform.position;
+
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, clampMin.position.x + halfwidth,clampMax.position.x - halfwidth);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, clampMin.position.y + halfheight, clampMax.position.y - halfheight);
+
+        transform.position = clampedPosition;
+
     }
 }
